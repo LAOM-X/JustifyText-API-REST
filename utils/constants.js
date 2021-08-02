43fce87ada5cbe6a07dec8b38dc11deed6141b6f
marvin <marvinazowe1999@gmail.com>
2021-08-02 16:59:45 +0200

    updating constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,7 @@
 const NB_MAX_WORD = 1000;
 const NB_MAX_CHAR_LINE = 80;
-const PERCENT_WORD_JUSTIFY = 0.80
 const constants = {
     NB_MAX_WORD, 
-    NB_MAX_CHAR_LINE,
-    PERCENT_WORD_JUSTIFY
+    NB_MAX_CHAR_LINE
 }
 module.exports = constants;
